6b1cd72813b8ac6fc47503131114e874fba3b4fa
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 15:44:55 +0300

    2.1

--- src/ArrayStats.java
+++ src/ArrayStats.java
@@ -1,2 +1,14 @@
 public class ArrayStats {
+
+    public static int getMax(int[] numbers) {
+        int max = numbers[0];
+
+        for (int n : numbers) {
+            if (n > max)
+                max = n;
+        }
+
+        return max;
+    }
+
 }
