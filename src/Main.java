1ce31340427eade7e149dfccda79d1faf996bbae
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 15:57:03 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    static void main() {
+        int[] nums = {4, 8, 3, 10, 17};
+        System.out.println(ArrayStats.getMax(nums));
+        System.out.println(ArrayStats.getMin(nums));
+        System.out.println(ArrayStats.getMaxSum(nums));
+        System.out.println(ArrayStats.getMinSum(nums));
+    }
 }
